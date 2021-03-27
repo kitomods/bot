b6c79abd7a91946cec4b0bba51800be49c4044c4
kitomods <78054628+kitomods@users.noreply.github.com>
2021-03-27 13:39:13 -0300

    update menuadm.js

--- src/menuadm.js
+++ src/menuadm.js
@@ -7,7 +7,7 @@ const menuadm = (prefix, pushname) => {
  ├─ ❏ ${prefix}add 5521....
  ├─ ❏ ${prefix}abrirgrupo
  ├─ ❏ ${prefix}promover
- ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}sair
  ├─ ❏ ${prefix}rebaixar
  ├─ ❏ ${prefix}marcar
  └─ ❏ ${prefix}fechargrupo
