4e8f884b3c6903e0553c4d24a0564393af9eb7d3
kitomods <78054628+kitomods@users.noreply.github.com>
2021-05-15 23:37:56 -0300

    update index.js

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const {
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = require('./lib/color')
 const { help } = require('./src/help')
-const { menuadm } = require('./src/menuadm')
-const { kitomenu } = require('./src/kitomenu')
-const { tabela } = require('./src/tabela')
 const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, banner, start, info, success, close } = require('./lib/functions')
 const { fetchJson, fetchText } = require('./lib/fetcher')
 const { recognize } = require('./lib/ocr')
+const fs = require('fs')
 const moment = require('moment-timezone')
 const { exec } = require('child_process')
 const fetch = require('node-fetch')
+const tiktod = require('tiktok-scraper')
 const ffmpeg = require('fluent-ffmpeg')
 const { removeBackgroundFromImageFile } = require('remove.bg')
 const lolis = require('lolis.life')
@@ -23,17 +22,7 @@ const loli = new lolis()
 const welkom = JSON.parse(fs.readFileSync('./src/welkom.json'))
 const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
-
-/******LOAD OF VCARD INPUT******/
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
-            + 'VERSION:3.0\n' 
-            + 'FN:kito\n' // full name
-            + 'ORG:rude\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=556296638900:+55 21 8763-5924\n' // ID do WhatsApp + número de telefone
-            + 'END:VCARD'
-/******END OF VCARD INPUT******/
-
-prefix = '/'
+prefix = '.'
 blocked = []
 
 function kyun(seconds){
@@ -104,7 +93,8 @@ async function starts() {
 	    }
 	})
 
-	client.on('group-participants-update', async (anu) => {
+	client.on('chat-update', async (mek) => {
+		try {
                         if (!mek.hasNewMessage) return
                         mek = JSON.parse(JSON.stringify(mek)).messages[0]
 			if (!mek.message) return
