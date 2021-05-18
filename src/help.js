23dcda50b6c07ee0c5782e96b59d62acb3d13d64
kitomods <78054628+kitomods@users.noreply.github.com>
2021-05-18 12:04:41 -0300

    update help.js

--- src/help.js
+++ src/help.js
@@ -10,7 +10,7 @@ ${prefix}𝚌𝚛𝚒𝚊𝚍𝚘𝚛
 ${prefix}𝚔𝚒𝚝𝚘𝚖𝚎𝚗𝚞
 ${prefix}𝚖𝚎𝚗𝚞𝚊𝚍𝚖
 ${prefix}𝚊𝚍𝚖𝚒𝚗𝚜
-${prefix}f𝚏𝚒𝚐
+${prefix}𝚏𝚒𝚐
 ${prefix}𝚙𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
 ${prefix}𝚑𝚊𝚙𝚙𝚢𝚖𝚘𝚍
 ${prefix}𝚙𝚕𝚊𝚢𝚜𝚝𝚘𝚛𝚎
@@ -21,7 +21,8 @@ ${prefix}𝚝𝚘𝚒𝚖𝚐
 ${prefix}𝚖𝚎𝚖𝚎
 ${prefix}𝚝𝚝𝚜
 ${prefix}𝚊𝚝𝚝𝚙
-${prefix}𝚠𝚊𝚒𝚝
+${prefix}𝚐𝚘𝚘𝚐𝚕𝚎
+${prefix}𝚋𝚊𝚗𝚕𝚒𝚜𝚝
 ${prefix}𝚕𝚒𝚗𝚔
 ◤◢◣◥◤◢◣◥◤◢◣◥ ◤◢◣◥◤◢◣◥◤◢◣◥ 
 `
