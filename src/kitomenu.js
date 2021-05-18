7f89ac61c16a624024e1a061e1aa167f74972d7a
kitomods <78054628+kitomods@users.noreply.github.com>
2021-05-18 12:03:09 -0300

    update kitomenu.js

--- src/kitomenu.js
+++ src/kitomenu.js
@@ -2,8 +2,11 @@ const kitomenu = (prefix) => {
 	return `
  ◪COMANDO DOS ADMINS
  │
+ ├─ ❏ ${prefix}sair
  ├─ ❏ ${prefix}prefixo
  ├─ ❏ ${prefix}limpar
+ ├─ ❏ ${prefix}ban
+ ├─ ❏ ${prefix}unban
  ├─ ❏ ${prefix}bc
  └─ ❏ ${prefix}novafoto
 `
